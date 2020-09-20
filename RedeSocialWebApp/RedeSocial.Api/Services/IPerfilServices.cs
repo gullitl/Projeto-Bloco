7d2a302a3ef9bb5f46c39b03da8aa5c44b3a1c9c
plusembo <plam.l@live.fr>
2020-09-20 12:14:37 -0400

    Merge both WebApp and Api projects

--- RedeSocialWebApp/RedeSocial.Api/Services/IPerfilServices.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RedeSocial.Api.Services
-{
-    public interface IPerfilServices
-    {
-        void Alterar(string id, PerfilRequest perfil);
-        PerfilResponse BuscarPerfil(string username);
-        void Cadastrar(PerfilRequest perfil);
-    }
-}
